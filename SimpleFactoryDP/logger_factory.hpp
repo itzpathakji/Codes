f4229b9e874954427c5f18347fcedf3f2ac87768
Shubham Kumar Pathak <shubhampathak26092002@gmail.com>
2024-06-14 15:18:15 +0530

    .hppInfo

--- SimpleFactoryDP/logger_factory.hpp
+++ SimpleFactoryDP/logger_factory.hpp
@@ -10,3 +10,19 @@ class LoggerFactory{
     static ILogger* createLogger(LogLevel plogLevel);  
     //returning a interface and if add another logger then nothing is going to change.
 };
+
+
+
+// The .hpp file extension is commonly used in C++ programming to denote a C++ header file. Header files in C++ typically contain declarations for functions, classes, and variables, which can then be included in multiple source files (.cpp files). Here are some key points about .hpp files:
+
+// Purpose: The .hpp file extension is used to differentiate C++ header files from C header files, which usually use the .h extension. This differentiation helps in organizing code and making it clear that the header is intended for C++.
+
+// Contents:
+
+// Declarations of functions, classes, and variables.
+// Include guards or #pragma once to prevent multiple inclusions.
+// Template definitions (since templates need to be defined in headers due to their nature).
+// Usage:
+
+// Included in C++ source files (.cpp files) using the #include directive.
+// Helps in separating interface (declaration) from implementation, promoting modularity and reusability.
